107a24ca6d8b3950f450b68324cc5a9c2ff2f6cf
Dikendev <diken.dev@gmail.com>
2023-12-05 23:09:55 -0300

    add swagger annotations

--- src/share/dtos/posts/create-post.dto.ts
+++ src/share/dtos/posts/create-post.dto.ts
@@ -1,7 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreatePostDto {
   id?: number;
 
+  @ApiProperty({
+    example: 1,
+  })
   userId: number;
+
+  @ApiProperty({
+    example: 'Post title',
+  })
   title: string;
+
+  @ApiProperty({
+    example: 'Post content',
+  })
   content: string;
 }
